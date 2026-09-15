dd8624893841050a8db624c06a227d53d39aac3e
Zsolt421 <maraczi.zsolt2007@gmail.com>
2026-09-15 16:06:38 +0200

    Added selection, crossover and data structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import os
+import random
 pygame.init()
 
 WIDTH = 1000
@@ -9,6 +10,12 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 generation_counter = 1
 pygame.display.set_caption(f"Iteration: {generation_counter}")
 FPS = 60
+NUMBER_OF_ENTITIES = 100
+NUMBER_OF_GENES = 16
+GEN_MIN = -1
+GEN_MAX = 1
+TOURNAMENT_SIZE = 5
+cars = []
 
 folder = os.path.dirname(__file__)
 
@@ -28,8 +35,7 @@ car_length = 50
 car_width = 30
 
 car_surface = pygame.Surface(
-    (car_length, car_width),
-    pygame.SRCALPHA
+    (car_length, car_width), pygame.SRCALPHA
 )
 
 car_surface.fill((220, 20, 20))
@@ -49,12 +55,71 @@ wheel_distance = 40
 MAX_SPEED = 150
 TURN_SPEED = 60
 
-## main loop
+
+class Car:
+    def __init__(self, genome):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = MAX_SPEED
+        self.alive = True
+        self.fitness = 0
+        self.genome = genome
+
+def first_creation(NUMBER_OF_ENTITIES, NUMBER_OF_GENES) -> None:
+
+    for j in range(NUMBER_OF_ENTITIES):
+        genome = []
+
+        for i in range(NUMBER_OF_GENES):
+            genome.append(random.uniform(GEN_MIN, GEN_MAX))
+
+        cars.append(Car(genome))
+
+def genome_crosser(parent1, parent2) -> list:
+    child_genes = []
+
+    for i in range(len(parent1.genome)):
+        choice = random.randint(1, 2)
+
+        if choice == 1:
+            gene = parent1.genome[i]
+            child_genes.append(gene)
+        else:
+            gene = parent2.genome[i]
+            child_genes.append(gene)
+
+    return child_genes
+
+def tournament_selection(cars, tournament_selection_constant) -> Car:
+    competitors = random.sample(cars, tournament_selection_constant)
+    king = competitors[0]
+
+    for k in range(1, tournament_selection_constant):
+        if competitors[k].fitness > king.fitness:
+            king = competitors[k]
+
+    return king
+
+
+def genetic_algorithm_tournament_selection(cars) -> list:
+    parent1 = tournament_selection(cars, TOURNAMENT_SIZE)
+    parent2 = tournament_selection(cars, TOURNAMENT_SIZE)
+
+    while parent1 is parent2:
+        parent2 = tournament_selection(cars, TOURNAMENT_SIZE)
+
+    child_genes = genome_crosser(parent1, parent2,)
+
+    return child_genes
+
 
 def main():
     global x, y, angle
     clock = pygame.time.Clock()
     run = True
+
+    first_creation(NUMBER_OF_ENTITIES, NUMBER_OF_GENES)
 
     while run:
         dt = clock.tick(FPS)/1000
@@ -117,7 +182,7 @@ def main():
         SCREEN.blit(rotated_car, rect)
 
         # Show finished frame
-        pygame.display.flip()      
+        pygame.display.flip()
 
     pygame.quit()
 
